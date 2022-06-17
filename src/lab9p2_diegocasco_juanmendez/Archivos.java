50f5d243be6e46669cc1e777d512b09c847a9fb6
dcasco02 <dcasco@unitec.edu>
2022-06-17 13:54:09 -0600

    Clases

--- src/lab9p2_diegocasco_juanmendez/Archivos.java
+++ src/lab9p2_diegocasco_juanmendez/Archivos.java
@@ -10,5 +10,55 @@ package lab9p2_diegocasco_juanmendez;
  * @author dcasc
  */
 public class Archivos {
-    
+    private String nombre;
+    private String link;
+    private String extension;
+    private int tamano;
+
+    public Archivos() {
+    }
+
+    public Archivos(String nombre, String link, String extension, int tamano) {
+        this.nombre = nombre;
+        this.link = link;
+        this.extension = extension;
+        this.tamano = tamano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    @Override
+    public String toString() {
+        return "Archivos{" + "nombre=" + nombre + ", link=" + link + ", extension=" + extension + ", tamano=" + tamano + '}';
+    }
 }
--- src/lab9p2_diegocasco_juanmendez/Carpeta.java
+++ src/lab9p2_diegocasco_juanmendez/Carpeta.java
@@ -5,10 +5,61 @@
  */
 package lab9p2_diegocasco_juanmendez;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author dcasc
  */
 public class Carpeta {
+    private String nombre;
+    private String link;
+    ArrayList <Carpeta> carpetas = new ArrayList();
+    ArrayList <Archivos> archivo = new ArrayList();
+
+    public Carpeta() {
+    }
+
+    public Carpeta(String nombre, String link) {
+        this.nombre = nombre;
+        this.link = link;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public ArrayList<Carpeta> getCarpetas() {
+        return carpetas;
+    }
+
+    public void setCarpetas(ArrayList<Carpeta> carpetas) {
+        this.carpetas = carpetas;
+    }
+
+    public ArrayList<Archivos> getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(ArrayList<Archivos> archivo) {
+        this.archivo = archivo;
+    }
+
+    @Override
+    public String toString() {
+        return "Carpeta{" + "nombre=" + nombre + ", link=" + link + ", carpetas=" + carpetas + ", archivo=" + archivo + '}';
+    }
     
 }
